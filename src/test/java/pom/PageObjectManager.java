84aff2ef1370397154c16980d15420d9d58b980a
SHIVAGAMIKP <shivagami.parthiban@gmail.com>
2026-02-01 13:59:19 -0500

    Implement PageObjectManager for page management

--- src/test/java/pom/PageObjectManager.java
+++ src/test/java/pom/PageObjectManager.java
@@ -1,1 +1,39 @@
+package pom;
+
+import org.openqa.selenium.WebDriver;
+
+import pages.ActivityInsightsMenuPage;
+import pages.LaunchPage;
+
+public class PageObjectManager {
+
+	private WebDriver driver;
+	private LaunchPage launchPage;
+	private ActivityInsightsMenuPage activityInsightsMenuPage;
+
+	public PageObjectManager(WebDriver driver) {
+		// driverFactory = new DriverFactory();
+		// driver = driverFactory.getDriver();
+		this.driver = driver;
+
+	}
+
+	public LaunchPage getLaunchPage() {
+
+		if (launchPage == null) {
+			launchPage = new LaunchPage(driver);
+		}
+		return launchPage;
+
+	}
+
+	public ActivityInsightsMenuPage geActivityInsightsMenuPage() {
+
+		if (activityInsightsMenuPage == null) {
+			activityInsightsMenuPage = new ActivityInsightsMenuPage(driver);
+		}
+		return activityInsightsMenuPage;
+
+	}
+}
 
