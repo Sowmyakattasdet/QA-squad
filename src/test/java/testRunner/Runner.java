3d5264c69e0a23d1351e58cfc55cc4687db0e195
elavarasi2888 <elavarasi2888@gmail.com>
2026-02-05 21:24:40 -0500

    runner update

--- src/test/java/testRunner/Runner.java
+++ src/test/java/testRunner/Runner.java
@@ -7,7 +7,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/features", glue = {"hooks",
-		"stepDefinitions"}, tags = "@Run", plugin = {"pretty",
+		"stepDefinitions"},
+//tags = "@Run", 
+plugin = {"pretty",
 				"html:cucumber-reports.html",
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, dryRun = false, monochrome = true)
